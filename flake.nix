dcf9a946d242610b47e5727d573c39abd3873c34
Nathan Day <nathandotday98@gmail.com>
2025-12-20 23:41:26 +0000

    fastbuild stuff

--- flake.nix
+++ flake.nix
@@ -3,10 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    fastbuild = {
+      url = "https://fastbuild.org/downloads/v1.18/FASTBuild-Linux-x64-v1.18.zip";
+      # sha256 = "144d968vjhrjgjqnqqi288hw39x6qc28mmmizin84n8xijg5rirz";
+      flake = false;
+    };
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, fastbuild, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -18,6 +24,7 @@
           llvmPkgs.clang
           mold
           llvmPkgs.libcxx
+          "{fastbuild}/fbuild"
         ];
       };
     };
